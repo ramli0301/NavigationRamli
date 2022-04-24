76274a4cc73055bb565da50d0859d820ba9306d7
Ramli <ramlijmd123@gmail.com>
2022-04-24 09:37:33 +0800

    commit pertama

---
+++ lib/WrapWidget.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+class WrapWidget extends StatefulWidget {
+        @override
+        _WrapWidgetState createState() => _WrapWidgetState();
+    }
+      
+    class _WrapWidgetState extends State<WrapWidget> {
+      
+        late BuildContext context; //global context
+      
+        @override
+        Widget build(BuildContext context) {
+          this.context = context;
+          return Scaffold(
+            body: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(top: 16, bottom: 16),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              'Update Content',
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                 ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 12, right: 12, top: 8),
+                              child: Text(
+                                "Here we observe that the layout is overflowing to the right which results in a broken widget",
+                                style: TextStyle(
+                                    color: Colors.grey[400],
+                                    fontSize: 14.0,
+                                    fontStyle: FontStyle.italic,
+                                    ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+      
+                      ///This will create an overflow error
+                      Row(
+                        children: <Widget>[
+                          chipDesign("Food", Color(0xFF4fc3f7)),
+                          chipDesign("Lifestyle", Color(0xFFffb74d)),
+                          chipDesign("Health", Color(0xFFff8a65)),
+                          chipDesign("Sports", Color(0xFF9575cd)),
+                          chipDesign("Nature", Color(0xFF4db6ac)),
+                          chipDesign("Fashion", Color(0xFFf06292)),
+                          chipDesign("Heritage", Color(0xFFa1887f)),
+                          chipDesign("City Life", Color(0xFF90a4ae)),
+                          chipDesign("Entertainment", Color(0xFFba68c8)),
+                        ],
+                      ),
+                    ],
+                  ),
+      
+                  divider(context),
+      
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(bottom: 16),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              'Content Instagram',
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                  ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 12, right: 12, top: 8),
+                              child: Text(
+                                'Here we observe that chips are contained inside a Wrap '
+                                'widget. It adjusts all the children according to the space'
+                                'available and automatically wraps to the next line',
+                                style: TextStyle(
+                                    color: Colors.grey[400],
+                                    fontSize: 14.0,
+                                    fontStyle: FontStyle.italic,
+                                    ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+      
+                      ///This will handle itself to get rid of the overflow error
+                      Wrap(
+                        spacing: 0.0, // gap between adjacent chips
+                        runSpacing: 0.0, // gap between lines
+                        children: <Widget>[
+                          chipDesign("Food", Color(0xFF4fc3f7)),
+                          chipDesign("Lifestyle", Color(0xFFffb74d)),
+                          chipDesign("Health", Color(0xFFff8a65)),
+                          chipDesign("Sports", Color(0xFF9575cd)),
+                          chipDesign("Nature", Color(0xFF4db6ac)),
+                          chipDesign("Fashion", Color(0xFFf06292)),
+                          chipDesign("Heritage", Color(0xFFa1887f)),
+                          chipDesign("City Life", Color(0xFF90a4ae)),
+                          chipDesign("Entertainment", Color(0xFFba68c8)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+      }
+      
+      
+    ///Common method to design a chip with different properties
+    ///like label and background color
+    Widget chipDesign(String label, Color color) => Container(
+          child: Chip(
+            label: Text(
+                label,
+                style: TextStyle(
+                    color: Colors.white),
+              ),
+              backgroundColor: color,
+              elevation: 4,
+              shadowColor: Colors.grey[50],
+              padding: EdgeInsets.all(4),
+            ),
+            margin: EdgeInsets.only(left: 12, right: 12, top: 2, bottom: 2),
+          );
+      
+      ///Method to create a divider with added margin
+    Container divider(BuildContext context) => Container(
+        child: Divider(),
+        margin: EdgeInsets.only(left: 10, right: 10, top: 28, bottom: 28),
+    );
---
+++ lib/detail.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:postest4_1915026040_ramli/WrapWidget.dart';
+
+class Details extends StatelessWidget {
+  const Details({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+         title: Text(
+          "Main Page",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: WrapWidget(),
+      
+
+      drawer: Drawer(
+          child: ListView(
+        children: [
+          DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.amber,
+              ),
+              child: Text("1915026040_Ramli")),
+          ListTile(
+              leading: Icon(Icons.verified_user),
+              title: Text("Postest 3 "),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (_) {
+                    return MyHomePage();
+                  },
+                ));
+              })
+        ],
+      )),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+enum Gender { male, female }
+
+class _MyHomePageState extends State<MyHomePage> {
+  String name = "", address = "";
+
+  final nameController = TextEditingController();
+  final addressController = TextEditingController();
+  Gender? jenisKelamin = Gender.male;
+  bool? nilaiCheckbox = false;
+  List<String> content = ["Instagram", "Walpapers", "Banner", "Resume"];
+  String contentGroup = "";
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+
+    nameController.dispose();
+    addressController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text(
+          "Profile Canva App",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: ListView(
+        children: [
+          Divider(),
+          TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Name",
+            ),
+          ),
+          SizedBox(height: 20),
+          TextFormField(
+            maxLines: 1,
+            controller: addressController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Email Address",
+            ),
+          ),
+          Divider(),
+          Text(
+            "Genre",
+            style: TextStyle(fontSize: 14),
+          ),
+          ListTile(
+            leading: Radio(
+              value: Gender.male,
+              groupValue: jenisKelamin,
+              onChanged: (Gender? value) {
+                setState(() {
+                  jenisKelamin = value;
+                });
+              },
+            ),
+            title: Text("Male"),
+          ),
+          ListTile(
+            leading: Radio(
+              value: Gender.female,
+              groupValue: jenisKelamin,
+              onChanged: (Gender? value) {
+                setState(() {
+                  jenisKelamin = value;
+                });
+              },
+            ),
+            title: Text("Female"),
+          ),
+          Divider(),
+          Text("Browse Content"),
+          for (var item in content)
+            Row(
+              children: [
+                Radio(
+                  value: item,
+                  groupValue: contentGroup,
+                  onChanged: (newValue) {
+                    setState(() {
+                      contentGroup = newValue.toString();
+                    });
+                  },
+                ),
+                Text(item),
+              ],
+            ),
+          Container(
+            width: 200,
+            height: 50,
+            alignment: Alignment.center,
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  name = nameController.text;
+                  address = addressController.text;
+                });
+              },
+              child: Text("Save"),
+            ),
+          ),
+          Divider(),
+          Text(
+            "Your Name: ${name} ",
+          ),
+          Divider(),
+          Text(
+            "Your Address: ${address} ",
+          ),
+          Divider(),
+          Text(
+            "Your Genre: ${jenisKelamin} ",
+          ),
+          Divider(),
+          Text(
+            "Your Content: ${contentGroup} ",
+          )
+        ],
+      ),
+    );
+  }
+}
